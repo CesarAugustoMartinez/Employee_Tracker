a5fe2c738dcb6675f6a0b748a775eef4814582fb
Cesar Martinez <cesaguma@hotmail.com>
2020-11-08 21:50:18 -0500

    Creating a schema.sql file with SQL code to create the Database and its tables for dapartmets, roles, and employees

---
+++ db/schema.sql
@@ -0,0 +1,41 @@
+DROP DATABASE IF EXISTS employee_tracker_db;
+
+CREATE DATABASE employee_tracker_db;
+
+USE employee_tracker_db;
+
+CREATE TABLE deparment (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10,4) NOT NULL,
+  department_id INT NULL NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id) REFERENCES deparment(id)
+);
+
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NULL NULL,
+  manager_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
+
+
+
+
+
+-- INSERT INTO products (flavor, price, quantity)
+-- VALUES ("vanilla", 2.50, 100);
+
+
